7ca362cd9538642a06e38c760d7b7e3fc317cc29
Sean Yang <seanyixuan@gmail.com>
2021-11-30 19:11:09 -0800

    got backend POST working for signup

--- server/routes/signup.js
+++ server/routes/signup.js
@@ -8,25 +8,26 @@ const bcrypt = require("bcryptjs");
 router.post("/", (req, res) => {
     let username = req.body.user_name;
     let password = req.body.password;
-    // bcrypt
-    //   .hash(password, 8)
-    //   .then((hashPassword) => {
-    //     new User({ user_name: username, password: hashPassword })
-    //       .save()
-    //       .then((user) => {
-    //           console.log(user);
-    //         let token = jwt.sign(
-    //           { username: user.username, password: user.password },
-    //           process.env.JWT_SECRET,
-    //           { expiresIn: "24h" }
-    //         );
-    //         res.json({ token: token });
-    //       })
-    //       .catch((err) => {
-    //         res.status(400).send({ error: err.message });
-    //       });
-    //   })
-    //   .catch((err) => res.status(400).send({ error: err.message }));
+
+    bcrypt
+      .hash(password, 8)
+      .then((hashPassword) => {
+        new User({ user_name: username, password: hashPassword })
+          .save()
+          .then((user) => {
+              console.log(user);
+            let token = jwt.sign(
+              { username: user.username, password: user.password },
+              process.env.JWT_SECRET,
+              { expiresIn: "48h" }
+            );
+            res.json({ token: token });
+          })
+          .catch((err) => {
+            res.status(400).send({ error: err.message });
+          });
+      })
+      .catch((err) => res.status(400).send({ error: err.message }));
   });
 
 
