76d4992041fb5e45bb8d2f0cafb07ff4f2cbb514
Sean Yang <seanyixuan@gmail.com>
2021-11-30 20:18:13 -0800

    client side login authentication working

--- client/src/pages/Login/Login.js
+++ client/src/pages/Login/Login.js
@@ -3,12 +3,52 @@ import React, { Component } from "react";
 
 
 export default class Login extends Component {
-  
+  state = {
+    loginFormData: null,
+  };
+
+  handleChange = (event) => {
+    this.setState({
+      loginFormData: { ...this.state.loginFormData, [event.target.name]: event.target.value },
+    });
+  };
+
+  handleSubmit = (event) => {
+    event.preventDefault();
+    axios
+      .post("http://localhost:8080/login", this.state.loginFormData)
+      .then((response) => {
+        console.log(response.data);         
+        sessionStorage.setItem("token", response.data.token);         // using sessionStorage here instead of cookie 
+        this.props.history.push({ pathname: "/itinerary" });          // directing user to the itinerary page upon login
+      })
+      .catch((err) => console.error("Error", err));
+  };
 
   render() {
     return (
       <section className="login">
-       
+       <h1>Login</h1>
+        <article className="login__container">
+          <form
+            action=""
+            method="POST"
+            className="login__form"
+            onSubmit={this.handleSubmit}
+          >
+            <div className="login__usernameContainer">
+              <label>Username:</label>
+              <input name="user_name" onChange={this.handleChange} />
+            </div>
+            <div className="login__passwordContainer">
+              <label>Password:</label>
+              <input name="password" onChange={this.handleChange} />
+            </div>
+            <button type="submit" className="login__btn">
+              Login
+            </button>
+          </form>
+        </article>
       </section>
     );
   }
