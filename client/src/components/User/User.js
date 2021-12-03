f2b2ce7bb2b57a4837b6f0cadb0a84b536c1c7fd
Sean Yang <seanyixuan@gmail.com>
2021-12-02 21:48:44 -0800

    got all buttons to display on User page

--- client/src/components/User/User.js
+++ client/src/components/User/User.js
@@ -9,15 +9,7 @@ export default class User extends Component {
     noItinerary: false,
   };
 
-  // handleClick = (id) => {
-  //   this.props.history.push(`/itinerary/${id}`);
-  // };
-  // handleClickEdit = (id) => {
-  //   this.props.history.push(`/editItinerary/${id}`);
-  // };
-  // handleClickDelete = (id) => {
-  //   this.props.history.push(`/deleteItinerary/${id}`);
-  // };
+
 
   getUser = (id) => {
     axios
@@ -52,26 +44,22 @@ export default class User extends Component {
       
   };
 
-  // handleClickAdd = () => {
-  //   this.props.history.push(`/addItinerary/${this.props.match.params.id}`);
-  // };
+  handleClickAdd = () => {
+    this.props.history.push(`/addItinerary/${this.props.match.params.id}`);
+  };
+  handleClick = (id) => {
+    this.props.history.push(`/itinerary/${id}`);
+  };
+  handleClickEdit = (id) => {
+    this.props.history.push(`/editItinerary/${id}`);
+  };
+  handleClickDelete = (id) => {
+    this.props.history.push(`/deleteItinerary/${id}`);
+  };
 
   componentDidMount() {
     this.getUser(this.props.match.params.id);
-    // return axios
-    //   .get(`http://localhost:8080/users/singleuser/1`, {
-    //     headers: {
-    //       authorization: `Bearer ${sessionStorage.getItem("token")}`,
-    //     },
-    //   })
-    //   .then((res) => {
-    //     console.log(res);
-    //     this.setState({
-    //       token: [res.data],
-    //     });
-    //     this.getUser(this.props.match.params.id);
-    //   })
-    //   .catch((err) => console.error({ message: err }));
+   ;
   }
 
   componentDidUpdate(prevProps) {
@@ -86,41 +74,32 @@ export default class User extends Component {
     console.log(sessionStorage);
     return (
       <section className="user">
-        <article>
-          
-          {this.state.user.map((user) => (
-            <article key={user.id}>
-              <article className="user__headingContainer">
-                <h2 className="user__username">
-                  {/* {user.user_name}'s User */}
-                  {/* {console.log(user.id, sessionStorage.id)} */}
-                  {user && 
-
-                  user.id == sessionStorage.id 
+      <article>
+        {this.state.user.map((user) => (
+          <article key={user.id}>
+            <article className="user__headingContainer">
+              <h2 className="user__username">
+                {user &&
+                user.id == sessionStorage.id
                   ? "My Profile"
-                  : `${user.user_name}'s Profile`
-                  }
-                  {/* {this.state.token[0].user_name &&
-                  user.user_name === this.state.token[0].user_name
-                    ? "My User"
-                    : `${user.user_name}'s User`} */}
-                </h2>
-                <h2 className="user__title">Itineraries</h2>
-              </article>
-              </article>
+                  : `${user.user_name}'s Profile`}
+              </h2>
+              <h2 className="user__title">Itinerary</h2>
+            </article>
 
-              /* {user.user_name === this.state.token[0].user_name ? (
-                <div className="user__btnContainer">
-                  <button
-                    onClick={this.handleClickAdd}
-                    className="user__btn--add"
-                  >
-                    Add An Itinerary
-                  </button>
-                </div>
-              ) : (
-                <></>
-              )}
+            {user.id == sessionStorage.id ? (
+              <div className="user__btnContainer">
+                <button
+                  onClick={this.handleClickAdd}
+                  className="user__btn--add"
+                >
+                  Add An Itinerary
+                </button>
+              </div>
+            ) : (
+              <></>
+            )}
+
 
               <article className="user__container">
                 {user.itinerary.map((itinerary) => (
@@ -145,9 +124,10 @@ export default class User extends Component {
                                 Check Out My Itinerary
                               </button>
                               <span>
-                                {this.state.token.map((user) =>
-                                  user.user_name === user.user_name ? (
+                                {this.state.user.map((user) =>
+                                  user.id == sessionStorage.id ? (
                                     <div key={itinerary.id}>
+                                      
                                       <button
                                         className="user__btn"
                                         onClick={() =>
@@ -178,12 +158,12 @@ export default class User extends Component {
                   </article>
                 ))}
               </article>
-              </article> */
-          ))} 
-        </article>
-      </section>
-    );
-  }
+              </article>
+        ))}
+      </article>
+    </section>
+  );
+}
 }
 
 
