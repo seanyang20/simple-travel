6fa6f79c881569bfb65204e71d7d0a76f16e86b6
Sean Yang <seanyixuan@gmail.com>
2021-12-04 18:12:02 -0800

    created homepage hero backgroud video

--- client/src/pages/Home/Home.js
+++ client/src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Home.scss";
 import { Link, useHistory} from "react-router-dom";
+import travel from "../../assets/videos/travel.mp4";
 
 export default function Home() {
     const history = useHistory();
@@ -11,6 +12,9 @@ export default function Home() {
   return (
     <header className="home">
       <article className="home__textContainer">
+        <video id="videoBG" poster={travel} autoPlay muted loop>
+            <source src={travel} type="video/mp4"/>
+        </video>
         <h1 className="home__title">SimpleTravel</h1>
         <p className="home__text">Find or create your own ideal travel itinerary</p>
         <article>
